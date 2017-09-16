5b22755e815dce3b49df64477e60518d2bdd0d6a
Dimitrios Desyllas <ddesillas@freemail.gr>
2017-09-16 18:49:20 +0300

    variables works

--- variables.cpp
+++ variables.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main()
 {
   int a;
-  cout<<"HEY, you, I'm alive! Oh,and Hello World!\n";
-  cin << a;
-  cout << "A number "<< a
+  cout << "HEY, you, I'm alive! Oh,and Hello World!\n";
+  cin >> a;
+  a*=9;
+  cout << "A number times 9: " << a << "\n";
+  cin.get();
   return 1;
 }
