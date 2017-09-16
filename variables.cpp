291741c521de66324c5e1bdbbaf7f4b4838e4ac2
Dimitrios Desyllas <ddesillas@freemail.gr>
2017-09-16 19:03:06 +0300

    Uncommited commits

--- variables.cpp
+++ variables.cpp
@@ -7,7 +7,11 @@ int main()
   cout << "HEY, you, I'm alive! Oh,and Hello World!\n";
   cin >> a;
   a*=9;
-  cout << "A number times 9: " << a << "\n";
+  if(a>18){
+    cout << "A number times 9 bigger 18: " << a << "\n";
+  } else {
+    cout << "A number tims 9 less that 18: " << a << "\n";
+  }
   cin.get();
   return 1;
 }
