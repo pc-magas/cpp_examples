645953606b39b4b3572b7a6016a1ac0f51e9510a
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-10-07 19:38:03 +0300

    Testing function in C++

---
+++ functionTEst.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int power(int num);
+
+int power(int num){
+  return num*num;
+}
+
+int main()
+{
+  int i=10;
+  cout <<power(i)<<endl;
+  cin.get();
+}
