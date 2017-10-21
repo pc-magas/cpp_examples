f770b514182d8fad4a82b1fd217a40d6e9409268
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-10-21 18:58:45 +0300

    Pointer example work

---
+++ pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x; // A normal integer
+  int *p; // A pointer to an integer
+  p = &x; // Read it, "assign the address of x to p"
+  cout<<"Please enter a number"<<endl;
+  cin>> x; // Put a value in x, we could also use *p here
+  cin.ignore();
+  cout<< "Tne number " << *p <<" is stored in " << p << " memory address " <<endl; // Note the use of the * to get the value
+  cin.get();
+}
